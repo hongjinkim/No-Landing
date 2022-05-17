d7439aa9c14264f42768634de9e2270178df75ab
kd9811 <89845701+kd9811@users.noreply.github.com>
2022-05-18 00:25:44 +0900

    시네마틱 1-1 수정

--- Saved/VideoCaptures/MasterSequence1-1.bat
+++ Saved/VideoCaptures/MasterSequence1-1.bat
@@ -1,1 +1,5 @@
+<<<<<<< Updated upstream
 rv -nomb -fullscreen -noBorders -fps 30.000000 Shot1-1_0.mp4 Shot1-1_1.mp4 Shot1-1_2.mp4
+=======
+rv -nomb -fullscreen -noBorders -fps 60.000000 Shot1-1_1.mp4 Shot1-1_2.mp4
+>>>>>>> Stashed changes
